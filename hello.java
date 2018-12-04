ffb8a8231bd79d5b21e84fbedcb725249bb6d9a9
QALLIDI <qallidimohammedamine@gmail.com>
2018-12-04 14:36:34 +0000

    updated 2

--- hello.java
+++ hello.java
@@ -1,7 +1,6 @@
 
 public class hello {
     public static void main(String[] args){
-        private String nom;
         System.out.println("Hello, World !");
         System.out.println("Coucou !");
 
