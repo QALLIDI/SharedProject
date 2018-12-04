4fa6784bee311366be82b7a7af92b32701ddaf64
QALLIDI <qallidimohammedamine@gmail.com>
2018-12-04 14:42:16 +0000

    update 3

--- hello4.java
+++ hello4.java
@@ -1,4 +1,5 @@
 public class hello4 {
+    private final String nom = "";
     public static void main(String[] args){
         System.out.println("Hello, World 4!");
         System.out.println("Coucou 4!");
