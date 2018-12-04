120b2ab8d66ed7ac1b1e3e2b065b435cd036cfdd
QALLIDI <qallidimohammedamine@gmail.com>
2018-12-04 07:50:46 +0000

    first commit

---
+++ hello2.java
@@ -0,0 +1,7 @@
+public class hello2 {
+    public static void main(String[] args){
+        System.out.println("Hello, World 2!");
+        System.out.println("Coucou 2!");
+        System.out.println("Coucou 2! , hello !");
+    }
+}
