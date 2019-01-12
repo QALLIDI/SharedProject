ccf6cf2d284b6d3ba7bbd249132270e21cc00c53
Qallidi Mohammed AMine <qallidimohammedamine@gmail.com>
2019-01-12 18:24:07 +0000

    commit hello3.java

--- hello3.java
+++ hello3.java
@@ -1,5 +1,6 @@
 public class hello3 {
     public static void main(String[] args){
+        private static String nom ="Ahmed";
         System.out.println("Hello, World 3 !");
         System.out.println("Coucou 3 !");
 
